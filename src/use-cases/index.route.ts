50f92dc3f85db7b58a92c4294b71d4d25f05d0ad
Lindsay <51120285+loslds@users.noreply.github.com>
2025-01-09 18:26:10 -0300

    3

--- src/use-cases/index.route.ts
+++ src/use-cases/index.route.ts
@@ -4,10 +4,15 @@ import { userRoute } from './user/user.route';
 
 import { pessoaRoute } from './pessoa/pessoa.route';
 
+import { moduloRoute } from './modulo/modulo.route';
+
 const indexRoute = Router();
 
 indexRoute.use('/user', userRoute);
 
 indexRoute.use('/pessoa', pessoaRoute);
+
+indexRoute.use('/modulo', moduloRoute);
+
 
 export { indexRoute };
