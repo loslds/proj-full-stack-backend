dbb5eec71db6b89ae60414013987821c7ea98d7d
Lindsay <51120285+loslds@users.noreply.github.com>
2025-09-29 19:09:49 -0300

    a

--- src/use-cases/cliente/index.ts
+++ src/use-cases/cliente/index.ts
@@ -1,3 +1,19 @@
+//use-cases/ccliente/index.ts
 
-export { clientesRepository, clientesRoute } from './clientes.route';
-export type { ClientesRepository } from './clientes.repository';
+import { ClientesRepository } from './clientes.repository';
+
+export { clientesRoute } from './clientes.route';
+export { ClientesController } from './clientes.controller';
+export type { ClientesDto } from './clientes.dto';
+export { ClientesEntity } from './clientes.entity';
+export { ClientesRepository } from './clientes.repository';
+export * from './clientes.dto';
+export * from './clientes.validation';
+
+export const clientesConfig = {
+  tableName: 'clientes',
+  repoClass: ClientesRepository,
+  createMethod: 'createNotExistsClientes',
+  findAll: 'findClientesAll',
+  //insertDefaults: 'insertDefaultImagens',
+};
