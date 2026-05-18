6000c91dafb8fe160bce9c0c964680fea8441cef
Lindsay <51120285+loslds@users.noreply.github.com>
2026-05-18 19:54:30 -0300

    18-05-2026

--- src/services/utility/imagens/imagensGridSelect.ts
+++ src/services/utility/imagens/imagensGridSelect.ts
@@ -1,11 +1,12 @@
 
 //C:\repository\proj-full-stack-backend\src\services\utility\imagens\imagensGridSelect.ts
-
 export const imagensGridSelect = `
 SELECT
   id,
   nome,
   tipo,
+  path_origem,
+  path_dest,
   svg
 
 FROM imagens
--- src/services/utility/imagens/mapImagensGridRow.ts
+++ src/services/utility/imagens/mapImagensGridRow.ts
@@ -1,11 +1,12 @@
 
 //C:\repository\proj-full-stack-backend\src\services\utility\imagens\mapImagensGridRow.ts
-
-export function mapImagensGridRow(row) {
+export function mapImagensGridRow(row: any) {
   return {
     id: row.id,
+    preview: row.svg,
     nome: row.nome,
     tipo: row.tipo,
-    svg: Boolean(row.svg),
+    path_origem: row.path_origem,
+    path_dest: row.path_dest
   };
 }
